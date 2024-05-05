44b85b6d2c0d434128e981e59448e284decb4756
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-05-05 14:25:41 +0300

    Continie

---
+++ main.py
@@ -0,0 +1,114 @@
+
+import webbrowser
+import telebot
+from telebot import types
+import sqlite3
+
+bot = telebot.TeleBot(token='7073439978:AAE5Fb3Gft9R1fUKvoHQ6tcWJX-0j9PaJ14')
+name = None
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn = sqlite3.connect('itprogger.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment PRIMARY KEY, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем! Введите Ваше имя')
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+def user_pass(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('itprogger.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users (name, pass) VALUES ('%s', '%s')" %(name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Список пользователей', callback_data='users'))
+    bot.send_message(message.chat.id, 'Пользователь зарегистрирован!', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('itprogger.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    info = ''
+    for user in users:
+        info += f'Имя: {user[1]}, пароль: {user[2]}\n'
+
+    cur.close()
+    conn.close()
+
+    bot.send_message(call.message.chat.id, info)
+
+# @bot.message_handler()
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     markup = types.ReplyKeyboardMarkup()
+#     btn1 = types.KeyboardButton('Перейти на сайт')
+#     markup.row(btn1)
+#     btn2 = types.KeyboardButton('Удалить фото')
+#     btn3 = types.KeyboardButton('Изменить текст')
+#     markup.row(btn2, btn3)
+#     file = open('./photo.png', 'rb')
+#     bot.send_photo(message.chat.id, photo=file, reply_markup=markup)
+#     # bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
+#     bot.register_next_step_handler(message, on_click)
+
+# def on_click(message):
+#     if message.text == 'Перейти на сайт':
+#         bot.send_message(message.chat.id, 'Website is open')
+#     elif message.text == 'Удалить фото':
+#         bot.send_message(message.chat.id, 'Delete')
+
+
+# @bot.message_handler(commands=['site', 'web'])
+# def send_welcome(message):
+#     webbrowser.open('https://www.youtube.com/watch?v=')
+
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}, добро пожаловать!')
+
+# @bot.message_handler()
+# def handle_message(message):
+#     if message.text.lower() == 'привет':
+#         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
+#     elif message.text.lower() == 'id':
+#         bot.reply_to(message, f'{message.from_user.id}')
+
+# @bot.message_handler(content_types=['photo'])
+# def get_photo(message):
+#     markup = types.InlineKeyboardMarkup()
+#     btn1 = types.InlineKeyboardButton('Перейти на сайт', url="https://telegra.ph/file")
+#     markup.row(btn1)
+#     btn2 = types.InlineKeyboardButton('Удалить фото', callback_data="delete")
+#     btn3 = types.InlineKeyboardButton('Изменить текст', callback_data="edit")
+#     markup.row(btn2,btn3)
+#     # markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://www.youtube.com/watch?v='))
+#     bot.reply_to(message, 'Какое красивое фото!', reply_markup=markup)
+#
+# @bot.callback_query_handler(func=lambda callback: True)
+# def callback_message(callback):
+#     if callback.data == "delete":
+#         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+#     elif callback.data == "edit":
+#         bot.edit_message_text('Edit text',callback.message.chat.id, callback.message.message_id)
+#
+bot.polling(none_stop=True)
